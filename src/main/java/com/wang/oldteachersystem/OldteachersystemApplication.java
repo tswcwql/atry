9ef716f2f481478c8d728b8535dfb4507b344224
wql <2579520424@qq.com>
2021-05-08 23:45:43 +0800

    init repo

---
+++ src/main/java/com/wang/oldteachersystem/OldteachersystemApplication.java
@@ -0,0 +1,13 @@
+package com.wang.oldteachersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldteachersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldteachersystemApplication.class, args);
+    }
+
+}
